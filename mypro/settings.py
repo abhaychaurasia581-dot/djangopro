5ffc76bbbd714371b530e828bc07f170cd9dc9c6
abhay <abhaychaurasia581@gmail.com>
2026-04-07 15:45:03 +0530

    Initial commit

--- mypro/settings.py
+++ mypro/settings.py
@@ -116,8 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # jaha tum CSS rakhte ho
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # production ke liye
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
